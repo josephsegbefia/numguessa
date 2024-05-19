afe7e00065e6d1588d48527c1713599738312b84
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-05-19 13:43:21 +0200

    Add Game over logic

--- App.js
+++ App.js
@@ -3,19 +3,34 @@ import { StyleSheet, ImageBackground, SafeAreaView } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 import { LinearGradient } from "expo-linear-gradient";
 import GameScreen from "./screens/GameScreen";
+import GamOverScreen from "./screens/GameOverScreen";
 import Colors from "./constants/colors";
 
 export default function App() {
   const [userNumber, setUserNumber] = useState(undefined);
+  const [gameIsOver, setGameIsOver] = useState(true);
 
   function pickedNumberHandler(pickedNumber) {
     setUserNumber(pickedNumber);
+    setGameIsOver(false);
+  }
+
+  function gameOverHandler() {
+    setGameIsOver(true);
   }
 
   let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
+
   if (userNumber) {
-    screen = <GameScreen userNumber={userNumber} />;
+    screen = (
+      <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />
+    );
   }
+
+  if (gameIsOver && userNumber) {
+    screen = <GamOverScreen />;
+  }
+
   return (
     <LinearGradient
       colors={[Colors.primary700, Colors.accent500]}
---
+++ screens/GameOverScreen.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+function GamOverScreen() {
+  return <Text>Game is Over</Text>;
+}
+
+export default GamOverScreen;
